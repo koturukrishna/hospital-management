77a6ab3da1101a036b8125fb0f87983336180d7b
krishna <krishnakoturu526@gmail.com>
2025-05-09 15:27:28 +0530

    server cors issues

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,8 +104,12 @@ export const login = asyncHandler(async (req, res) => {
 export const logoutUser = (req, res) => {
   res.cookie("jwt", "", {
     httpOnly: true,
+    secure: true, // Netlify uses HTTPS, so this must be true
+    sameSite: "None", // Required for cross-origin cookies
+    maxAge: 24 * 60 * 60 * 1000, // 1 day in ms
     expires: new Date(0),
   });
+
   res.status(200).json({ message: "Logged out successfully" });
 };
 
